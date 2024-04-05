294c34cec4c8fe977e47af1b872b86d888a24e60
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-05 13:35:48 -0300

    initial commit

---
+++ src/app/_components/exemple-component/exemple-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemple-component',
+  templateUrl: './exemple-component.component.html',
+  styleUrl: './exemple-component.component.css'
+})
+export class ExempleComponentComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExempleComponentComponent } from './_components/exemple-component/exemple-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExempleComponentComponent
   ],
   imports: [
     BrowserModule,
